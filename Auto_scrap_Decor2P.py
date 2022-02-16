3b247142add63751004eb56b4392f8048b24f1cf
Juan Felipe Monsalvo Salazar <70897284+jmonsa13@users.noreply.github.com>
2022-02-16 09:22:51 -0500

    Se agrega autorun script en python
    El script que se agrega realiza el scrapping de la web de decorceramica
    para two pieces y guarda los datos en un archivo .csv.
    
    También se actualiza el README file con más información

---
+++ Auto_scrap_Decor2P.py
@@ -0,0 +1,78 @@
+# Python project for pricing scripping of different sites
+# Creado por: Juan Monsalvo
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Libraries import
+# ----------------------------------------------------------------------------------------------------------------------
+import datetime
+import locale
+import os
+
+import numpy as np
+import pandas as pd
+import requests
+
+from bs4 import BeautifulSoup
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Configuration and Global Variables
+# ----------------------------------------------------------------------------------------------------------------------
+# Setting the local currency, using DE to be able to have thousands sep = . and decimal point = ,
+locale.setlocale(locale.LC_NUMERIC, "de_DE")
+
+# Obtener definiciones de la configuración actual
+configuracion = locale.localeconv()
+
+# Path definition of the .csv file
+output_path = './Data/Decorceramica_twopieces.csv'
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Main code
+# ----------------------------------------------------------------------------------------------------------------------
+# URL definitioin
+URL = "https://www.decorceramica.com/busca/?fq=C:15/16&fq=spec_fct_243:1%20Pieza?O=OrderByScoreDESC"
+page = requests.get(URL)
+
+# Charging the content of the page using beautifulsoup
+soup = BeautifulSoup(page.content, "html.parser")
+# print(soup.prettify())
+
+# Finding the id and items where the products are located
+results = soup.find(id="collections")
+toilets_elements = results.find_all("div", class_="item")
+
+# Scrapping the names and prices of every item
+data = []
+for elem in toilets_elements:
+    product_ref = elem.find("b", class_="product-name").text.strip()
+    try:
+        elem.find("span", class_="out-of-stock").text.strip()
+        if elem.find("span", class_="out-of-stock").text.strip() == "Disponible próximamente":
+            price_ref = elem.find("span", class_="out-of-stock").text.strip()
+
+            message_price = "El producto no tiene existencia actualmente."
+            message_priceIVA = " "
+            price_IVA = 0
+    except:
+        price_ref = elem.find("span",
+                              class_="best-price").text.strip()  # OJO el precio que se muestra aqui esta sin iva
+        raw_numbers = locale.atof(price_ref.strip("$"))
+
+        message_price = "El precio sin IVA es de: {}".format(price_ref)
+        message_priceIVA = "El precio con IVA es de: ${}".format(int(np.round(raw_numbers * 1.19)))
+        price_IVA = int(np.round(raw_numbers * 1.19))
+
+    # print("El sanitario es el: {}".format(product_ref))
+    # print(message_price)
+    # print(message_priceIVA)
+
+    # Appending the item in a list
+    data.append([datetime.datetime.today().date(), product_ref, price_IVA])
+
+# Creating the dataframe
+df = pd.DataFrame(data, columns=["Date", "Product", "Price"])
+
+# Saving the file in a .csv file
+df.to_csv(output_path, mode='a', header=not os.path.exists(output_path), index=False)
+
+
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Scrapping
-Código para el scrapping de paginas web para captura de precio
+# Pricing web Scrapping
+Código para el scrapping de páginas web para captura de precio de productos
+ceramicos de la competencia. Se capturan precios de las siguientes páginas:
+
+* https://www.decorceramica.com/busca/?fq=C:15/16&fq=spec_fct_243:1%20Pieza?O=OrderByScoreDESC
+* 
+
+
+Tambien se tiene la creación de un dashboard para presentación de los datos 
+usando streamlit.
