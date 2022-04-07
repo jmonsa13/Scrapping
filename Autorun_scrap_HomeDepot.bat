c1a468c749d18f70fa5545292ea33b8e78d16f65
jmonsalvo <jmonsa13@gmail.com>
2022-04-07 14:24:08 -0500

    Se crea el autoscript de homedepot para correr automaticamente

---
+++ Autorun_scrap_HomeDepot.bat
@@ -0,0 +1,3 @@
+set root=D:\Users\jmonsalvo\Anaconda3\
+call %root%\condabin\activate.bat %root%\envs\Machine_Learning
+call python "E:\OneDrive - Corona\03_Proy_ID\03_MACHINE_LEARNING\01_Projects\11_Web_Scrapping\Pricing\Scrapping_git\Auto_scrap_HomeDepot.py"
