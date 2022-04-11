9d5b546ab067c901f695f88f8cd4a73e9ea4666f
Juan Monsalvo <jmonsa13@gmail.com>
2022-04-11 16:29:50 -0500

    Se agregan columnas al Auto_scrap_HomeDepot.py

--- Auto_scrap_HomeDepot.py
+++ Auto_scrap_HomeDepot.py
@@ -30,7 +30,7 @@ fecha = datetime.datetime.today()
 output_path_toilet = './XX_Data/Homedepot_toilet-' + str(fecha.year) + '_' + str(fecha.month) + '.csv'
 
 # Path for loading the URL sites
-url_path_toilet = './XX_Url/HomeDepot_URL.csv'
+url_path_toilet = './XX_Url/HomeDepot_URL.xlsx'
 
 # Number of retry
 NUM_RETRIES = 5
@@ -49,7 +49,7 @@ headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleW
 # ----------------------------------------------------------------------------------------------------------------------
 # Function Definition
 # ----------------------------------------------------------------------------------------------------------------------
-def homedepot_data(url_link, soup_html):
+def homedepot_data(elem, soup_html):
     """
     Programa que toma la información general de una pagina de producto del market place de HomeDepot
     """
@@ -65,11 +65,28 @@ def homedepot_data(url_link, soup_html):
     else:
         product_format = "Toilet"
 
-    # Collecting the price
+    # Collecting the current price
     price_raw = soup_html.find("div", class_="price").text.strip()[:-2]
     decimals = float(soup_html.find("div", class_="price").text.strip()[-2:])
     price_sin_decimal = int(locale.atof(price_raw.strip("$")))
     price_clean = price_sin_decimal + (decimals/100)
+
+    # TODO: Capturar en promoción
+    # Como capturar un precio que muestre promoción / TOCA USAR SELENIUM
+
+    # # Collecting the was-price
+    # if soup_html.find("div", class_="price-detailed__was-price") is None:
+    #     was_price_clean = np.nan
+    # else:
+    #     print(soup_html.find("div", id="eco-rebate-price"))
+    #     print(soup_html.find("div", class_="price-detailed__was-price"))
+    #
+    #     was_price_raw = soup.find("span", class_="u__strike").text.strip()[:-2]
+    #     print(was_price_raw)
+    #     was_price_dec_raw = float(soup_html.find("div", class_="price-detailed__was-price").text.strip()[-2:])
+    #     print(was_price_dec_raw)
+    #     was_price_sin_decimal = int(locale.atof(was_price_raw.strip("$")))
+    #     was_price_clean = was_price_sin_decimal + (was_price_dec_raw/100)
 
     # Collecting the sku
     sku_ref_bar = soup_html.find_all("h2", class_="product-info-bar__detail--7va8o")
@@ -78,9 +95,15 @@ def homedepot_data(url_link, soup_html):
     sku_ref = ""
     for item_raw in sku_ref_bar:
         item = item_raw.text.strip().split(" #")[0]
-        if item == "Internet":
+        if item == "Model":
             sku_ref = item_raw.text.strip().split(" #")[1]
+        elif item == "Internet":
+            internet_ref = item_raw.text.strip().split(" #")[1]
 
+    # Todo: Tomar dato de envio
+    # Taking delivery cost
+    # delivery = soup_html.find("div", class_="buybox__car")
+    # print(delivery)
 
     # Collecting the image
     image_raw = soup_html.find("a", class_="mediagallery__anchor")
@@ -89,19 +112,21 @@ def homedepot_data(url_link, soup_html):
 
     # ------------------------------------------------------------------------------------------------------------------
     # Message display
-    print("Recopilando la información de {}".format(url_link))
+    print("Recopilando la información de {}".format(elem["Link"]))
     print("La marca es la: {}".format(brand_name))
     print("El sanitario es el: {}".format(product_name))
     print("El tipo de producto es un: {}".format(product_format))
     print("El sku es el: {}".format(sku_ref))
-    print("El precio es: {} USD".format(price_clean))
+    print("El precio actual es: {} USD".format(price_clean))
+    #print("El precio anterior es: {} USD".format(was_price_clean))
     print(url_img)
     print("\n")
     # ------------------------------------------------------------------------------------------------------------------
 
     # Appending the item in a list
-    information = [datetime.datetime.today().date(), brand_name, product_format, brand_name, product_name, sku_ref,
-                   price_clean, "USD", "homedepot.com", "Si", url_link, url_img]
+    information = [datetime.datetime.today().date(), brand_name, elem["Linea"], product_format, elem["Rough in"],
+                   elem["Bowl Height"], elem["Asiento / Capacidad"], product_name, sku_ref, internet_ref,
+                   price_clean, "USD", "homedepot.com", "Si", elem["Link"], url_img]
 
     return information
 
@@ -110,14 +135,17 @@ def homedepot_data(url_link, soup_html):
 # Main code
 # ----------------------------------------------------------------------------------------------------------------------
 for product_type, output_path in [[url_path_toilet, output_path_toilet]]:
-    # Reading .csv file with url list
-    url_list = pd.read_csv(product_type)
+    # Reading .xlsx file with url list
+    file_df = pd.read_excel(product_type)
 
-    # Scrapping the information of every url
+    # Keeping just the row with links
+    product_df = file_df[file_df['Link'].notna()]
+
+    # Scrapping the information of every product
     data = []
-    for url in url_list["URL_product"]:
+    for index, elem in product_df.iterrows():
         # send request to scraperapi, and automatically retry failed requests
-        params = {'api_key': API_KEY, 'url': url}
+        params = {'api_key': API_KEY, 'url': elem["Link"]}
 
         for i in range(NUM_RETRIES):
             # Random Wait between request
@@ -142,12 +170,13 @@ for product_type, output_path in [[url_path_toilet, output_path_toilet]]:
             soup: BeautifulSoup = BeautifulSoup(response.content, "html.parser")
 
             # Getting the information from the website
-            toilet_information = homedepot_data(url, soup)
+            toilet_information = homedepot_data(elem, soup)
             data.append(toilet_information)
 
     # Creating the dataframe
-    df = pd.DataFrame(data, columns=["Fecha", "Marca", "Tipo", "Familia", "Producto", "SKU", "Precio", "Moneda",
-                                     "Market_Place", "Stock", "URL", "Image_url"])
+    df = pd.DataFrame(data, columns=["Fecha", "Marca", "Linea", "Tipo", "Rough_In", "Bowl Height", "Asiento/Capacidad",
+                                     "Producto", "SKU", "Cod_Internet", "Precio", "Moneda", "Market_Place", "Stock",
+                                     "URL", "Image_url"])
 
     # Saving the file in a .csv file
     df.to_csv(output_path, mode='a', header=not os.path.exists(output_path), index=False)
